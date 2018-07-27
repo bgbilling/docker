1f76a29d501ede20e8646ff6621460b6a755e71e
amir <amir@bitel.ru>
2018-07-27 18:41:24 +0500

    split on two repo

--- stack/mysql/initdb.d/user.sql
+++
@@ -1,4 +0,0 @@
-GRANT ALL PRIVILEGES ON bgbilling.* TO 'bill'@'localhost' IDENTIFIED BY 'bgbilling';
-GRANT ALL PRIVILEGES ON bgbilling.* TO 'bill'@'%' IDENTIFIED BY 'bgbilling';
-GRANT PROCESS, RELOAD ON *.* TO 'bill'@'localhost';
-GRANT PROCESS, RELOAD ON *.* TO 'bill'@'%';
