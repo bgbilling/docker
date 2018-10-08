27704fa8eb26318daf59f8f2080f9b64ff81d150
amir <amir@bitel.ru>
2018-10-08 19:57:55 +0500

    centos support

--- install/activemq/5.15.5/activemq-debian.sh
+++ install/activemq/5.15.5/activemq-debian.sh
@@ -7,7 +7,8 @@ ACTIVEMQ_HOME=/opt/activemq/current
 ACTIVEMQ_URL=https://archive.apache.org/dist/activemq/$ACTIVEMQ_VERSION/$ACTIVEMQ-bin.tar.gz
 ACTIVEMQ_SHA512=450469a36147cef8dc3ef5f98de7e173dc63e5ca82e090417c112a70132b7a7da87851e330443e9d68f085c375f4e85f2cae34317d09a59295fec05dab8f2d26
 
-[ ! -f /opt/activemq ] \
+set -x \
+  && [ ! -f /opt/activemq ] \
   && mkdir -p /opt/activemq \
   && curl $ACTIVEMQ_URL -o /tmp/$ACTIVEMQ-bin.tar.gz \
   && sha512sum /tmp/$ACTIVEMQ-bin.tar.gz | grep $ACTIVEMQ_SHA512 \
@@ -22,4 +23,5 @@ ACTIVEMQ_SHA512=450469a36147cef8dc3ef5f98de7e173dc63e5ca82e090417c112a70132b7a7d
   && rm $ACTIVEMQ_HOME/activemq-all-$ACTIVEMQ_VERSION.jar \
   && sed -i 's@activemq.username=system@activemq.username=bill@' $ACTIVEMQ_HOME/conf/credentials.properties \
   && sed -i 's@activemq.password=manager@activemq.password=bgbilling@' $ACTIVEMQ_HOME/conf/credentials.properties \
+  && sed -i 's@0.0.0.0@127.0.0.1@' $ACTIVEMQ_HOME/conf/jetty.xml \
   && curl https://raw.githubusercontent.com/bgbilling/images-base/master/install/activemq/activemq.service -o /lib/systemd/system/activemq.service
--- install/wildfly/11.0.0/wildfly-debian.sh
+++ install/wildfly/11.0.0/wildfly-debian.sh
@@ -10,7 +10,8 @@ WILDFLY_MEMORY="-Xmx512m"
 WILDFLY_DIRECT_MEMORY="-XX:MaxDirectMemorySize=60m"
 WILDFLY_META_MEMORY="-XX:MaxMetaspaceSize=200m"
 
-[ ! -d $WILDFLY_HOME ] \
+set -x \
+  && [ ! -d $WILDFLY_HOME ] \
   && mkdir -p $WILDFLY_DIR \
   && curl https://download.jboss.org/wildfly/$WILDFLY_VERSION/wildfly-$WILDFLY_VERSION.tar.gz -o /tmp/wildfly-$WILDFLY_VERSION.tar.gz \
   && sha1sum /tmp/wildfly-$WILDFLY_VERSION.tar.gz | grep $WILDFLY_SHA1 \
